88fa2b1a09943a1d8f55a332dd65cf587df3ffad
Hung Co <hungco8923@gmail.com>
2024-10-25 18:23:01 +0700

    added json

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,9 @@ export default function Hero() {
     <div className="hero">
       <div className="container">
         <div className="info">
-          <h1>This is the love site of cún and cún</h1>
+          <h1 style={{ fontSize: "4.5rem" }}>
+            This is the love site of cún and cún
+          </h1>
           <h2>Where we begin our journey in 17th of June 2024</h2>
           <p>
             This site is updated at 25th of October 2024. It's been 4 months.
