c2b22e5a65d85582a65afe52ee86a70478e70ec0
Hung Co <hungco8923@gmail.com>
2024-10-26 10:54:03 +0700

    menu added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  // Lock scrolling when the menu is open
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+    return () => {
+      document.body.classList.remove("no-scroll"); // Cleanup on unmount
+    };
+  }, [menuOpen]);
+
   return (
-    <header className="header">
-      <Link to="/cunvacun/" className="logo">
-        &hearts
+    <header className={`header ${menuOpen ? "open" : ""}`}>
+      <Link
+        style={{ textDecoration: "none" }}
+        to="/cunvacun/"
+        className="logo"
+        onClick={() => setMenuOpen(false)}
+      >
+        Cun&Cun
       </Link>
 
-      <nav className="navbar">
-        <Link to="/cunvacun/">Home</Link>
-        <Link to="/cunvacun/gallery">Gallery</Link>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        {menuOpen ? "x" : "☰"}
+      </button>
+
+      <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+        <Link to="/cunvacun/" onClick={() => setMenuOpen(false)}>
+          Home
+        </Link>
+        <Link to="/cunvacun/gallery" onClick={() => setMenuOpen(false)}>
+          Gallery
+        </Link>
       </nav>
     </header>
   );
